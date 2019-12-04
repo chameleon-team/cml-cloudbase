15bf1a9210d804cbd222a3e8d14409de49df6f6b
JiM-W <550116990@qq.com>
2019-12-04 19:36:24 +0800

    init

---
+++ chameleon.config.js
@@ -0,0 +1,76 @@
+
+// 设置静态资源的线上路径
+const publicPath = '//www.static.chameleon.com/cml';
+// 设置api请求前缀
+const apiPrefix = 'https://api.chameleon.com';
+
+cml.config.merge({
+  check: {
+    enable: false,
+  },
+  base: {
+    dev: {
+      domain: {
+        domain1: "localhost",
+        domain2: "localhost"
+      },
+    },
+    build: {
+      domain: {
+        domain1: "http://api.cml.com",
+        domain2: "http://api2.cml.com"
+      },
+    }
+  },
+  templateLang: "cml",
+  templateType: "html",
+  platforms: ["web","weex","wx","alipay","baidu","qq"],
+  buildInfo: {
+    wxAppId: '123456'
+  },
+  wx: {
+    dev: {
+    },
+    build: {
+      apiPrefix
+    }
+  },
+  web: {
+    dev: {
+      analysis: false,
+      console: false
+    },
+    build: {
+      analysis: false,
+      publicPath: `${publicPath}/web/`,
+      apiPrefix
+    }
+  },
+  weex: {
+    dev: {
+    },
+    build: {
+      publicPath: `${publicPath}/weex/`,
+      apiPrefix
+    },
+    custom: {
+      publicPath: `${publicPath}/wx/`,
+      apiPrefix
+    }
+  },
+  cloudConfig:{
+    wx:{
+      test:222,
+    }
+  }
+})
+cml.utils.plugin('webpackConfig', function(params) {
+  let { type, media, webpackConfig } = params
+  if (type === 'wx') {
+    debugger;
+    console.log(cml.config.get().cloudConfig)
+    console.log(cml.projectRoot)
+  }
+  return { type, media, webpackConfig }
+})
+
